a552d592984631aaa99f1987a3e061159605ba6d
AmirHosseinMissMe <amirhossein2324236@gmail.com>
2021-06-21 00:18:41 -0700

    Comeplted predict and added some tests.

--- word_spot_service.py
+++ word_spot_service.py
@@ -1,10 +1,14 @@
-from numpy import np
+import numpy as np
 import tensorflow.keras as keras
+import librosa
+import sys
 
 # This is a singleton class :
 # Which means there is just one instance of it in program
 
 MODEL_PATH = "model.h5"
+NUM_SAMPLES = 22050
+SEPRATOR = ','
 
 class _WordSpotService:
     
@@ -56,8 +60,16 @@ class _WordSpotService:
         return predictions
 
 
-    def preprocess(self, file_path):
-        pass    
+    def preprocess(self, file_path, n_mfccs= 13, n_fft=2048, hop_length=512):
+        
+        signal, sr = librosa.load(file_path)
+
+        if len(signal) > NUM_SAMPLES:
+            signal = signal[:NUM_SAMPLES]
+
+        MFCCs = librosa.feature.mfcc(signal, n_mfcc=n_mfccs, n_fft=n_fft, hop_length=hop_length)
+
+        return MFCCs.T
 
 
 def word_spot_servie():
@@ -68,3 +80,18 @@ def word_spot_servie():
         _WordSpotService.model = keras.models.load_model(MODEL_PATH)
 
     return _WordSpotService._instance
+
+if __name__ == "__main__":
+
+    wss = word_spot_servie()
+
+    test_file_path = sys.argv[1]
+    test_file = open(test_file_path, 'r')
+    lines = test_file.readlines()
+
+    for line in lines:
+        if line[-1] == '\n':
+            line = line[:-1]
+        word, path = line.split(',')
+        path = "tests/" + path
+        print(f"Word: {word}, Predicted: {wss.predict(path)}")
